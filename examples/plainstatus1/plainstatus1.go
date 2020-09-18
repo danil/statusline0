355cc938b6df71dba664b20267aea3249768e972
Danil Kutkevich <danil@kutkevich.org>
2020-09-18 23:18:52 +0300

    docs: updating first example

--- examples/plainstatus1/plainstatus1.go
+++ examples/plainstatus1/plainstatus1.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"strings"
 	"syscall"
 	"time"
 
@@ -36,12 +40,28 @@ func main() {
 		return bytefmt.New(fs.Bavail * uint64(fs.Bsize))
 	}
 	tPth, _ := plainstatus.FileName("/sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input")
+	ctrNum := func() string {
+		c1 := exec.Command("docker", "ps", "--quiet")
+		c2 := exec.Command("wc", "--lines")
+		r, w := io.Pipe()
+		c1.Stdout = w
+		c2.Stdin = r
+		var b2 bytes.Buffer
+		c2.Stdout = &b2
+		c1.Start()
+		c2.Start()
+		c1.Wait()
+		w.Close()
+		c2.Wait()
+		return strings.TrimRight(b2.String(), "\n")
+	}
 	f := []func() string{
 		func() string { batt.Power, batt.Sign = plainstatus.BatteryPercent(); return fmt.Sprint(batt) },
 		func() string { return fmt.Sprintf("%4s", plainstatus.LoadAverage1(plainstatus.LoadAverage())) },
 		func() string { temp.Value = plainstatus.Temperature(tPth); return fmt.Sprint(temp) },
 		func() string { mfa := memFreeAvail(); return fmt.Sprintf(" %d~%d", mfa[:]...) },
 		func() string { df := diskAvail(); return fmt.Sprintf(" %d", df) },
+		func() string { n := ctrNum(); return " c" + n },
 		func() string { return time.Now().Local().Format(" Jan-02-Mon MST 15:04") },
 		func() string { return "      " },
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/danil/plainstatus
 
 go 1.13
 
-require github.com/danil/bytefmt v0.0.0-20200118151459-01f52b26a28b // indirect
+require (
+	github.com/danil/bytefmt v0.0.0-20200118151459-01f52b26a28b // indirect
+	github.com/shirou/gopsutil v2.20.8+incompatible // indirect
+	golang.org/x/sys v0.0.0-20200918174421-af09f7315aff // indirect
+)
